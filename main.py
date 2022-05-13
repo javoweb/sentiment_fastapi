9589dc6dd3201b0d9c5ce81a40a73b51e94c399e
Gonzalo Tixilima <g.tixilima@uniandes.edu.co>
2022-05-12 19:43:31 -0500

    rename endpoint

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def health():
     return "Service is online."
 
 @app.post("/sentiment")
-def my_endpoint(request: PredictionRequest):
+def sentiment_analysis_endpoint(request: PredictionRequest):
     sentiment_query_sentence = request.query_string
     sentiment = sentiment_model(sentiment_query_sentence)
     return sentiment
